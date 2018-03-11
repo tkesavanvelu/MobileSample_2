74fad38311a0ac6d15ac8ca1ec5603ebaaad8f00
kv_developer <kv_developer@users.noreply.github.com>
2018-03-11 17:18:51 -0500

    another sample

---
+++ App3/MainActivity.cs
@@ -0,0 +1,63 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Datalib;
+
+namespace App3
+{
+	[Activity(Label = "Kesavan", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+
+		Button btnNext;
+		Button btnPrev;
+		TextView txtTitle;
+		TextView txtDesc;
+
+
+		DataManager dMgr;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			btnPrev = FindViewById<Button>(Resource.Id.Prev);
+			btnNext = FindViewById<Button>(Resource.Id.Next);
+			txtTitle = FindViewById<TextView>(Resource.Id.Title);
+			txtDesc = FindViewById<TextView>(Resource.Id.Desc);
+
+
+			btnPrev.Click += BtnPrev_Click;
+			btnNext.Click += BtnNext_Click;
+
+			dMgr = new DataManager();
+			dMgr.MoveFirst();
+			UpdateUI();
+
+
+		}
+
+		private void BtnNext_Click(object sender, System.EventArgs e)
+		{
+			dMgr.MoveNext();
+			UpdateUI();	
+			
+		}
+
+		private void BtnPrev_Click(object sender, System.EventArgs e)
+		{
+			dMgr.MovePrev();
+			UpdateUI();
+		}
+
+		private void UpdateUI()
+		{
+			txtTitle.Text = dMgr.Current.Title;
+			txtDesc.Text = dMgr.Current.Description;
+		}
+	}
+}
+
---
+++ Datalib/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Datalib
+{
+	public class Class1
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+
+	}
+
+	public class DataManager
+	{
+		private readonly Class1[] DataElems;
+		private int currentIndex;
+		public DataManager()
+		{
+			DataElems = InitData();
+		}
+
+		private Class1[] InitData()
+		{
+			var InitData = new Class1[] {
+				new Class1
+				{
+					Title = "Engineering",
+					Description = "Engineering Team , Responsibility: Design, Develop, Test "
+				},
+				new Class1
+				{
+					Title = "Marketing",
+					Description = "Marketing Team,Responsibility : Sales , Presentation"
+				},
+				new Class1
+				{
+					Title = "Accounting",
+					Description = "Accounting Team, Responsibility : Accounts, Payroll, AR, AP"
+				},
+				new Class1
+				{
+					Title = "Project Management",
+					Description = "Project Management, Responsibility : Projects, PMO , Delivery"
+				},
+				new Class1
+				{
+					Title = "Quality Assurance",
+					Description = "Quality Assurance Team,Responsibilty : Testing,Automation "
+				}
+			};
+			return InitData;
+		}
+		public void MoveFirst()
+		{
+			currentIndex = 0;
+		}
+
+		public void MovePrev()
+		{
+			if (currentIndex > 0)
+				--currentIndex;
+		}
+
+		public void MoveNext()
+		{
+			if  ( currentIndex < DataElems.Length -1  )
+				++currentIndex;
+		}
+
+		public Class1 Current
+		{
+			get { return DataElems[currentIndex]; }
+		}
+
+	}
+}
